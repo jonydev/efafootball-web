5262b2863e960776947970df7d75239fb1a86ddc
晴识明月 <1593674921@qq.com>
2016-12-16 20:06:33 +0800

    10

--- js/team.js
+++ js/team.js
@@ -8,7 +8,10 @@ $(document).ready(function (){
 $(".search-div").click(function () {
         $(".search-top").addClass("hidden");
 });
-
+$(document).on("click",".team-ul li",function () {
+    var team_id=$(this).attr("id");
+    window.location.href="http://120.76.206.174:8080/efafootball-web/team-detail.html?id="+team_id;
+})
 function searchTeambyName() {
     //添加商品搜索处理
     alert("点击自动搜索商品");
