7e9494a311c738fd83a6c494a96da4e6bda8b5dc
晴识明月 <1593674921@qq.com>
2017-04-18 09:37:50 +0800

    1

--- js/mine-login.js
+++ js/mine-login.js
@@ -15,11 +15,16 @@ $(".login-btn").click(function () {
         success:function (data) {
             var login_success=eval(data.result);
             if(login_success){            //login success
-                localStorage.setItem("have_logined",1);
-                //把json数据转换成字符串格式存储
-                localStorage.setItem("mine_info",JSON.stringify(data.rows[0]));
-                localStorage.setItem("loginId",data.rows[0].id);
-                window.location.href="http://120.76.206.174:8080/efafootball-web/mine.html";
+                try{
+                    localStorage.setItem("have_logined",1);
+                    //把json数据转换成字符串格式存储
+                    localStorage.setItem("mine_info",JSON.stringify(data.rows[0]));
+                    localStorage.setItem("loginId",data.rows[0].id);
+                    window.location.href="http://120.76.206.174:8080/efafootball-web/mine.html";
+                }
+                catch (e){
+                    TIP_ERROR("无法写localstorage...");
+                }
             }else{                       //login failed
                 $(".Tip").removeClass("hidden");
                 setTimeout('$(".Tip").addClass("hidden")',1500);
